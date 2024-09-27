2fd1c7c76aa3447e5b0c662628093a9241854ef0
RodryJR <dariordguezll@gmail.com>
2024-09-27 03:20:11 -0400

    update startup

--- startup.sh
+++ startup.sh
@@ -1,14 +1,23 @@
 .startup.sh
 #!/bin/bash
 
-# Check if the correct number of arguments is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <path_to_csv_file>"
-    exit 1
+
+# Instalar las dependencias desde requirements.txt (si no existe, crea uno con 'pip freeze > requirements.txt')
+if [ -f "requirements.txt" ]; then
+    pip install -r requirements.txt
 fi
 
-# Assign the first argument to a variable
-CSV_FILE_PATH=$1
+# Obtener la ruta del directorio donde está ubicado el script
+SCRIPT_DIR=$(dirname "$0")
+# Cambiar al directorio donde está tu aplicación Flask
+# Cambiar al directorio donde está tu aplicación Flask
+cd "$SCRIPT_DIR"
 
-# Execute the Python program with the provided CSV file path
-python create_result.py "$CSV_FILE_PATH"
+cd src/Webapp/
+
+# Exportar la variable de entorno FLASK_APP y ejecutar la aplicación en modo de desarrollo
+export FLASK_APP=main.py
+export FLASK_ENV=development
+
+# Ejecutar la aplicación Flask
+flask run --host=0.0.0.0 --port=5000
